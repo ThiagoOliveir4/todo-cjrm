7e2077024106ac40befeff71107ceaedf31e3019
ThiagoOliveir4 <48225591+ThiagoOliveir4@users.noreply.github.com>
2020-05-28 21:54:50 -0300

    Update app.js
    Olá Roger, talvez essas mudanças sejam úteis. Não está implementado a procura de To-do's ainda.

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 const formAddTodo = document.querySelector('.form-add-todo')
+const containerTodo = document.querySelector('.todos-container')
+const DeleteTodoIcon = document.querySelectorAll('.delete')
+const inputTodo = document.querySelector('[name="add"]')
+
+const HandleFormSubmit = event => {
+
+    event.preventDefault()
+    const typedTodo = inputTodo.value.trim()
+    const isEmpty = typedTodo === ''
+    
+    if(isEmpty){
+        alert('Adicione a To-Do no campo !')
+        return
+    }
+    AddTodo( typedTodo )
+    ClearInputTodo()
+}
+
+const ClearInputTodo = ( ) => {
+    inputTodo.value ='';
+}
+
+
+const AddTodo = ( value ) => {
+    const li = document.createElement('li')
+    const span = document.createElement('span')
+    const i = document.createElement('i')
+
+    li.classList.add('list-group-item',
+        'd-flex','justify-content-between','align-items-center')
+    i.classList.add('far','fa-trash-alt','delete')
+    span.innerText= value
+    i.setAttribute('onclick','DeleteTodo(this)')
+    li.appendChild(span)
+    li.appendChild(i)
+
+    containerTodo.appendChild(li)
+}
+
+const DeleteTodo = ( event ) =>  {
+ 
+    const liTargetDelete = event.parentNode
+   containerTodo.removeChild(liTargetDelete)
+    
+}
+formAddTodo.addEventListener('submit',HandleFormSubmit)
